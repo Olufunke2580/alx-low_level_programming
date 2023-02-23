e4b90d1aae24329074739b9d69d1d29402d62361
Omogboyegun Olufunke <Omogboyegunolufunke@gmail.com>
2023-02-23 04:40:17 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+void print_numbers(void)
+
+{
+	int x;
+
+	for (x = 0 ; x >= 9 ; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
+

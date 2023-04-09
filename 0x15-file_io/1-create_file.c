0ea999571a1307903607b4a5a9e5e4013f8cdc74
Omogboyegun olufunke <omogboyegunolufunke@gmail.com>
2023-04-09 16:32:18 -0700

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,12 +16,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	fun = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fun = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	f = write(fun, text_content, len);
 	if (fun == -1 || f == -1)
 		return (-1);
